3389ac503634c96aebd58432230b0e44d84a1b8d
Your Name <you@example.com>
2019-05-17 20:00:23 +0400

    barev arev

---
+++ script.js
@@ -0,0 +1,115 @@
+// var matrix = [];
+// var n = 20;
+// var m = 20;
+// side = 20;
+
+// function setup() {
+//     for (var y = 0; y < n; y++) {
+//         matrix[y] = [];
+//         for (var x = 0; x < m; x++) {
+//             matrix[y][x] = random([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2]);
+//         }
+//     }
+//     console.log(matrix);
+//     createCanvas(matrix[0].length*side,matrix.length*side);
+// }
+// function draw() {
+//     for (var y = 0; y < matrix.length; y++) {
+//         for (var x = 0; x < matrix[0].length; x++) {
+//             if (matrix[y][x] == 1) {
+//                 fill("green");
+//                 rect(x * side, y * side, side, side)
+//             }
+//             else if (matrix[y][x] == 0) {
+//                 fill("grey");
+//                 rect(x * side, y * side, side, side)
+//             }
+//             else if (matrix[y][x] == 2) {
+//                 fill("yellow");
+//                 rect(x * side, y * side, side, side)
+//             }
+//         }
+//     }
+// }
+
+
+
+// var matrix = [
+//     [0, 0, 1, 0, 4],
+//     [1, 0, 2, 0, 0],
+//     [0, 1, 0, 5, 0],
+//     [0, 0, 1, 0, 0],
+//     [1, 1, 0, 0, 0],
+//     [1, 1, 0, 0, 0],
+//     [1, 1, 0, 0, 3]
+// ];
+var side = 50;
+var socket = io()
+// side = 10;
+function setup() {
+    createCanvas(20 * side, 20 * side);
+    background('#acacac');
+
+    //     for (var y = 0; y < matrix.length; y++) {
+    //         for (var x = 0; x < matrix[y].length; x++) {
+    //             if (matrix[y][x] == 1) {
+    //                 matrix[y][x] = new Grass(x, y, 1);
+    //             }
+    //             if (matrix[y][x] == 2) {
+    //                 matrix[y][x] = new GrassEater(x, y, 2);
+    //             }
+    //             if (matrix[y][x] == 3) {
+    //                 matrix[y][x] = new gishatich(x, y, 3);
+    //             }
+    //             if (matrix[y][x] == 4) {
+    //                 matrix[y][x] = new Sun(x, y, 4);
+    //             }
+    //             if (matrix[y][x] == 5) {
+    //                 matrix[y][x] = new ZarmanaliKerpar(x, y, 5);
+    //             }
+    //         }
+    //     }
+    //     console.log(matrix);
+}
+
+
+function draw() {
+}
+
+function drawMatrix(matrix) {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+            if (matrix[y][x] == 0) {
+                fill("grey");
+                // rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x].index == 1) {
+                fill("green");
+                // rect(x * side, y * side, side, side);
+            }
+            else if (matrix[y][x].index == 2) {
+                fill("yellow");
+                // rect(x * side, y * side, side, side);
+                matrix[y][x].acted = false;
+            }
+            else if (matrix[y][x].index == 3) {
+                fill("blue");
+                // rect(x * side, y * side, side, side);
+                matrix[y][x].acted = false;
+            }
+            else if (matrix[y][x].index == 4) {
+                fill("orange");
+                // rect(x * side, y * side, side, side);
+                matrix[y][x].acted = false;
+            }
+            else if (matrix[y][x].index == 5) {
+                fill("pink");
+                // rect(x * side, y * side, side, side);
+                matrix[y][x].acted = false;
+            }
+            rect(x * side, y * side, side, side);
+        }
+    }
+}
+socket.on("matrix", drawMatrix);
+
---
+++ server.js
@@ -0,0 +1,146 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require('fs');
+
+app.use(express.static("."));
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+server.listen(3000, function () {
+    console.log("port is running")
+});
+
+// Aystex kapum enq classery
+var Grass = require("./modul/Grass.js");
+var gishatich = require("./modul/gishatich.js");
+var GrassEater = require("./modul/GrassEater.js");
+var Sun = require("./modul/Sun.js");
+var ZarmanaliKerpar = require("./modul/ZarmanaliKerpar.js");
+
+
+// haytararum enq zangvacnery
+grassArr = [];
+grasseaterArr = [];
+gishatichArr = [];
+SunArr = [];
+ZarmanaliKerparArr = [];
+
+// Matrix enq haytararum
+
+var w = 50;
+var h = 60;
+
+function genMatrix(w, h) {
+    var matrix = [];
+    for (var y = 0; y < h; y++) {
+        matrix[y] = [];
+        for (var x = 0; x < w; x++) {
+            var r = Math.floor(Math.random() * 100);
+            if (r < 20) r = 0;
+            else if (r < 40) r = 1;
+            else if (r < 42) r = 2;
+            else if (r < 75) r = 3;
+            else if (r < 85) r = 4;
+            else if (r < 100) r = 5;
+            matrix[y][x] = r;
+        }
+    }
+    return matrix;
+}
+
+
+// st. enq zan.-ic patahakan  andam tvox function
+// Random = function (arr) {
+//     return arr[Math.floor(Math.random() * arr.length)];
+// }
+
+// kanchum enq getMatrix functiony ev talis enq Matrix popoxakanin
+matrix = genMatrix(w, h);
+
+// Aystex pttvum en matrix-i mijov e stexcum en objectnery
+// matrix = genMatrix(w, h);
+for (var y = 0; y < matrix.length; y++) {
+    for (var x = 0; x < matrix[y].length; x++) {
+        if (matrix[y][x] == 1) {
+            grassArr.push(new Grass(x, y, 1));
+        }
+        if (matrix[y][x] == 2) {
+            matrix[y][x] = new GrassEater(x, y, 2);
+        }
+        if (matrix[y][x] == 3) {
+            matrix[y][x] = new gishatich(x, y, 3);
+        }
+        if (matrix[y][x] == 4) {
+            matrix[y][x] = new Sun(x, y, 4);
+        }
+        if (matrix[y][x] == 5) {
+            matrix[y][x] = new ZarmanaliKerpar(x, y, 5);
+        }
+    }
+}
+
+// stexcum enq function vor kkanchi objectneri methodnery ev kuxarki matrixi masin datan script.js
+
+function drawserver() {
+
+    for (var i in grassArr) {
+        grassArr[i].mul();
+    }
+
+    for (var i in grasseaterArr) {
+        grasseaterArr[i].move();
+        grasseaterArr[i].mul();
+        grasseaterArr[i].eat();
+        grasseaterArr[i].die();
+    }
+
+    for (var i in gishatichArr) {
+        gishatichArr[i].move();
+        gishatichArr[i].mul();
+        gishatichArr[i].eat();
+        gishatichArr[i].die();
+    }
+
+    for (var i in SunArr) {
+        SunArr[i].move();
+        SunArr[i].mul();
+        SunArr[i].eat();
+        SunArr[i].die();
+    }
+
+    for (var i in ZarmanaliKerparArr) {
+        ZarmanaliKerparArr[i].move();
+        ZarmanaliKerparArr[i].mul();
+        ZarmanaliKerparArr[i].eat();
+        ZarmanaliKerparArr[i].die();
+    }
+    // matrixy uxarkum enq clientin
+    io.sockets.emit("matrix", matrix);
+}
+setInterval(drawserver, 100);
+Random = function (arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+
+// for (var y = 0; y < matrix.length; y++) {
+//     for (var x = 0; x < matrix[y].length; x++) {
+//         if (matrix[y][x] == 1) {
+//             matrix[y][x].mul();
+//         }
+//         if (matrix[y][x] == 2) {
+//             matrix[y][x].eat();
+//         }
+//         if (matrix[y][x] == 3) {
+//             matrix[y][x].eat();
+//         }
+//         if (matrix[y][x] == 4) {
+//             matrix[y][x].eat();
+//         }
+//         if (matrix[y][x] == 5) {
+//             matrix[y][x].xotstexcel();
+//         }
+//     }
+// }
